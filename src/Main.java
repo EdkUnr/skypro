9be9ffb0b7875fcb5d99d2a4e71d806ec96fce62
EdkUnr <richardlvov1780@gmail.com>
2023-11-29 17:47:56 +1000

    Homework theme "conditionals2"

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import com.sun.source.tree.IfTree;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World");
     byte clientOS = 1;
     if (clientOS == 1) {
         System.out.println("Установите версию приложения для Android по ссылке.");
